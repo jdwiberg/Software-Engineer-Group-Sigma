225a009ed1174149ed769347e186b1cb93afb692
Jacob Wiberg <jacobwiberg@Jacobs-MacBook-Air-674.local>
2025-11-23 20:44:58 -0500

    show receipts use case

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -44,7 +44,7 @@ export default function Login() {
             setMessage(body.message)
             setUsername("")
             setPassword("")
-            router.push('/shopperLists')
+            router.push('/shopperDashboard')
         }
     } catch (err) {
         console.error("something went wrong: ", err);
---
+++ frontend/app/shopperDashboard/History.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { useEffect, useState } from 'react'
+
+export default function History() {
+
+    return (
+    <div>
+        Put some history here
+    </div>
+    )
+}
+
