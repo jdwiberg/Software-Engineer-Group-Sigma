e4a9f2b32a7480aa79b1cae52346d49f896fddbf
Tgalica05 <timothygalica@gmail.com>
2025-11-25 10:33:27 -0500

    added remove shopping list

---
+++ backend/lib/remShoppingList/remShoppingList.mjs
@@ -0,0 +1,44 @@
+import * as mysql2 from 'mysql2'
+
+var pool
+
+let remShoppingList = (sl_id) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`DELETE FROM shoppingList WHERE sl_id = ?;`, [sl_id], (error, results) => {
+            if (error){
+                return reject(error)
+            }
+            resolve(results)
+        })
+    })
+}
+
+export const handler = async (event) =>{
+    let result
+    let code
+
+    pool = mysql2.createPool({
+        host: process.env.rdsHost,
+        user: process.env.rdsUser,
+        password: process.env.rdsPassword,
+        database: process.env.rdsDatabase
+    });
+
+    try {
+        const listItems = await remShoppingList(event.sl_id)
+        result = { message: "removed shopping list"}
+        code = 200
+
+        } catch (err) {
+        result = { error: err.message }
+        code = 400
+        }
+
+    const response = {
+        statusCode: code,
+        body: JSON.stringify(result)
+    }
+
+  pool.end()
+  return response
+};
