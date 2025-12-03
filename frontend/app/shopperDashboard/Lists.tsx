f146d245716ac096606c41b61df4e11ccb863b8b
Tgalica05 <timothygalica@gmail.com>
2025-12-03 13:09:08 -0500

    fixed get list function

--- frontend/app/shopperDashboard/Lists.tsx
+++ frontend/app/shopperDashboard/Lists.tsx
@@ -9,22 +9,25 @@ export default function Lists() {
         sl_date: string
     }
     type listItem = {
+        sli_id : number,
         sli_name : string,
         sli_category : string
     }
     const [error, setError] = useState("")
     const [message, setMessage] = useState("")
     const [username, setUsername] = useState("")
-    const [listName, setListName] = useState("") //for adding a new list
-    const [isDeleting, setIsDeleting] = useState(false)
-    const [shoppingLists, setShoppingLists] = useState<shoppingList[]>([])
-    const [shoppingListItems, setShoppingListItems] = useState<listItem[]>([])
+    const [shoppingLists, setShoppingLists] = useState<shoppingList[]>([])      // for veiwing shopping lists
+    const [shoppingListItems, setShoppingListItems] = useState<listItem[]>([])  // for veiwing an individual shopping list
     const [open, setOpen] = useState(false)
+    const [listName, setListName] = useState("")                                //for adding a new list
+    const [isDeleting, setIsDeleting] = useState(false)                         // for deleting a shopping list
     const [selectedList, setSelectedList] = useState<shoppingList | null>(null) // for selecting a list to display items
     const [isSubmitting, setIsSubmitting] = useState(false)
-    const [isAdding, setIsAdding] = useState(false)// for adding a new item to a list
+    const [isAdding, setIsAdding] = useState(false)                             // for adding a new item to a list
     const [itemName, setItemName] = useState("")
     const [itemCat, setItemCat] = useState("")
+    const [selectedItem, setSelectedItem] = useState<listItem | null>(null)     // for removing a list item
+    const [isRemoving, setIsRemoving] = useState(false)                         
     const categories = [
         "Alocohol & Spirits",
         "Baking Supplies",
@@ -90,13 +93,13 @@ export default function Lists() {
       }
     }
 
-    async function showItems(sl_name : string) {  
+    async function showItems(sl_id : number) {  
       try {
           const res = await fetch(
               "https://nsnnfm38da.execute-api.us-east-1.amazonaws.com/prod/getList", 
                 {
                     method: "POST",
-                    body: JSON.stringify({ username, sl_name })
+                    body: JSON.stringify({ username, sl_id })
                 }
           )
 
@@ -233,7 +236,7 @@ export default function Lists() {
                 setMessage("Some error")
             } else {
                 if (selectedList) {
-                    await showItems(selectedList.sl_name)
+                    await showItems(selectedList.sl_id)
                 }
                 setMessage(body.message)
             }
@@ -243,6 +246,44 @@ export default function Lists() {
             setItemName("")
             setItemCat("")
             setIsAdding(false)
+        }
+      }
+
+    async function removeListItem(sli_id : number) {
+        setMessage("")
+        setError("")
+        setIsRemoving(true)
+    
+        try {
+            const res = await fetch(
+                "https://nsnnfm38da.execute-api.us-east-1.amazonaws.com/prod/remListItem",
+                {
+                    method: "POST",
+                    body: JSON.stringify({ sli_id })
+                }
+            )
+            
+            const data = await res.json()
+    
+            let body
+            try {
+              body = JSON.parse(data.body);
+            } catch (err) {
+              console.error("Failed to parse body", err);
+            }
+    
+            if (data.statusCode != 200) {
+                setError(data.error)
+                setMessage("Some error")
+            } else {
+                setMessage(body.message)
+                await showItems(selectedList!.sl_id)
+            }
+        } catch (err) {
+            console.error("something went wrong: ", err);
+        } finally {
+            setIsRemoving(false)
+            setSelectedItem(null)
         }
       }
 
@@ -279,7 +320,7 @@ export default function Lists() {
                 <button
                     onClick={() => {
                         setSelectedList(shoppingList);
-                        showItems(shoppingList.sl_name);
+                        showItems(shoppingList.sl_id);
                         setOpen(true);
                     }}
                     className="px-4 py-2 bg-blue-500 text-white rounded"
@@ -311,7 +352,19 @@ export default function Lists() {
                     <li key={i} className="border-b py-2">
                         <strong>Item:</strong> {item.sli_name} <br />
                         <strong>Category:</strong> {item.sli_category}
+                    <button
+                        type='button'
+                        disabled={isRemoving  && selectedItem?.sli_id === item.sli_id}
+                        onClick={() => {
+                            setSelectedItem(item)
+                            removeListItem(item.sli_id)
+                            showItems(selectedList.sl_id)
+                        }}
+                    >
+                        {isRemoving  && selectedItem?.sli_id === item.sli_id ? "Removing... " : "Remove item"}
+                    </button>
                     </li>
+
                     ))}
                 </ul>
 
