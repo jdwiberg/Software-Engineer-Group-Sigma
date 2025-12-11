fd651f4699526427ea1721ad0ec73c1b64d21c8f
Jacob Wiberg <jacobwiberg@Jacobs-MacBook-Air-674.local>
2025-12-11 16:33:30 -0500

    fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export"
+  output: "export",
+  basePath: '/ShopComp'
 };
 
 export default nextConfig;
